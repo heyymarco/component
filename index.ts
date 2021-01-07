f9dee057060503564c87559524bfaab3d94e8521
heyymarco <yunuzk@gmail.com>
2021-01-07 22:21:29 +0630

    fix bugs & add features

--- index.ts
+++ index.ts
@@ -20,6 +20,12 @@ export default class Control extends Element {
     }
 
 
+    get markActive() : boolean {
+        return this.is(".active, .actived");
+    }
+    set markActive(active : boolean) {
+        this.removeClass("actived").toggleClass("active", active);
+    }
     get markPassive() : boolean {
         return this.is(".passive");
     }
@@ -57,6 +63,32 @@ export default class Control extends Element {
 
 
     /**
+     * get the state of activability of current control.
+     * @returns true indicates the current control is activated, or false indicates the current control is deactivated.
+     */
+    get active() : boolean {
+        return this.is(":active, :checked, .active, .actived");
+    }
+
+    /**
+     * set the state of activability of current control.
+     * @param active : set true to activate this element, or false to deactivate it.
+     */
+    set active(active : boolean) {
+        if (active) {
+            let items = this.filter(":not(:active):not(:checked):not(.active):not(.actived)");
+            items.markPassive = false; items.markActive = true;
+            items.trigger("change");
+        } else {
+            let items = this.filter(":active, :checked, .active, .actived");
+            items.markActive = false; items.markPassive  = true;
+            items.trigger("change");
+        }
+    }
+
+
+
+    /**
      * get the state of disability of current control.
      * @returns true indicates the current control is disabled, or false indicates the current control is not disabled (enabled).
      */
@@ -70,13 +102,16 @@ export default class Control extends Element {
      */
     set disabled(disabled : boolean) {
         if (disabled) {
-            this.filter(":not(:disabled)").prop("disabled", true);
-            this.markEnabled  = false; this.markDisabled = true;
-            this.trigger("disabled");
+            let items = this.filter(":not(:disabled)");
+            items.prop("disabled", true);
+            items.markEnabled  = false; items.markDisabled = true;
+            items.trigger("disabled");
         } else {
-            this.filter(":disabled")      .prop("disabled", false);
-            this.markDisabled = false; this.markEnabled  = true;
-            this.trigger("enabled");
+            let items = this.filter(":disabled");
+            items.prop("disabled", false);
+            items.markDisabled = false; items.markEnabled  = true;
+            if (items.is(".actived")) items.removeClass("actived").addClass("active");
+            items.trigger("enabled");
         }
     }
 
